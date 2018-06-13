0fe3020a120de5c9391883dfd50e85bdee7a2282
Adrian Balazs <adrian1468@gmail.com>
2018-06-13 22:54:48 +0200

    Solved fizzBuzz problem

---
+++ src/main/java/com/epam/mentoring/basicJavaExercises/FizzBuzz.java
@@ -0,0 +1,25 @@
+package com.epam.mentoring.basicJavaExercises;
+
+public class FizzBuzz {
+
+//   Write a method that prints the numbers between 1 and 100, but:
+//  *if a number is divisible by 3 then print "Fizz" instead of the number,
+//  *if a number is divisible by 5 then print "Buzz" instead of the number,
+//  *if a number is divisible by 3 and 5 then print "Fizz Buzz" instead of the number.
+
+    public void solveFizzBuzzProblem(){
+        for(int i=1; i<=100; i++){
+            if(i%3==0 && i%5==0){
+                System.out.print("FizzBuzz, ");
+            }else if(i%3==0){
+                System.out.print("Fizz, ");
+            }else if(i%5==0){
+                System.out.print("Buzz, ");
+            }
+            else{
+                System.out.print(i + ", ");
+            }
+        }
+
+    }
+}
--- src/main/java/com/epam/mentoring/basicJavaExercises/Main.java
+++ src/main/java/com/epam/mentoring/basicJavaExercises/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         UniqueNumbers uniqueNumbers = new UniqueNumbers();
         uniqueNumbers.createThreeDigitNums();
+
+        System.out.println();
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.solveFizzBuzzProblem();
     }
 }
