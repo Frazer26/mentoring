098dece9202463b536c7fa79bd5615706c74a2ce
Adrian Balazs <adrian1468@gmail.com>
2018-06-13 23:17:18 +0200

    Started "what percentage passed since the start of the year" exercise and formatted code of FizzBuzz class

--- src/main/java/com/epam/mentoring/basicJavaExercises/FizzBuzz.java
+++ src/main/java/com/epam/mentoring/basicJavaExercises/FizzBuzz.java
@@ -7,16 +7,15 @@ public class FizzBuzz {
 //  *if a number is divisible by 5 then print "Buzz" instead of the number,
 //  *if a number is divisible by 3 and 5 then print "Fizz Buzz" instead of the number.
 
-    public void solveFizzBuzzProblem(){
-        for(int i=1; i<=100; i++){
-            if(i%3==0 && i%5==0){
+    public void solveFizzBuzzProblem() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz, ");
-            }else if(i%3==0){
+            } else if (i % 3 == 0) {
                 System.out.print("Fizz, ");
-            }else if(i%5==0){
+            } else if (i % 5 == 0) {
                 System.out.print("Buzz, ");
-            }
-            else{
+            } else {
                 System.out.print(i + ", ");
             }
         }
---
+++ src/main/java/com/epam/mentoring/basicJavaExercises/Year.java
@@ -0,0 +1,10 @@
+package com.epam.mentoring.basicJavaExercises;
+
+public class Year {
+    //Write a method that takes a year, month, day, hour, minute as input and prints
+    //what percentage passed since the start of the year! Leap years should be taken into account!
+
+    public Boolean isLeapYear(int year) {
+
+    }
+}
