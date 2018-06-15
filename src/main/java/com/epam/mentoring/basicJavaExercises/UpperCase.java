5a07e2f41d4223ae7de6ed29731fd9412e275da6
Adrian Balazs <adrian1468@gmail.com>
2018-06-15 22:46:03 +0200

    Started UpperCase exercise

---
+++ src/main/java/com/epam/mentoring/basicJavaExercises/UpperCase.java
@@ -0,0 +1,14 @@
+package com.epam.mentoring.basicJavaExercises;
+
+public class UpperCase {
+
+    //Write a method that takes a string (a sentence) as input and converts the first letter of the words to uppercase!
+    public String convertUpperCase(String sentence) {
+        try {
+
+        } catch (NullPointerException e) {
+            System.err.println("Please give me a sentence! \n" + e.getMessage());
+        }
+        return "";
+    }
+}
