9916fd8be04df660262e6fa1c26731bde3d69528
Adrian Balazs <adrian1468@gmail.com>
2018-06-17 15:10:36 +0200

    Refactored Reversal class

--- src/main/java/com/epam/mentoring/basicJavaExercises/Reversal.java
+++ src/main/java/com/epam/mentoring/basicJavaExercises/Reversal.java
@@ -12,19 +12,22 @@ public class Reversal {
 
     //Write an application that reads the content of a file, reverses it and writes the result into a new file!
 
-    public void writeToFile(String file, String str){
+    public void writeToFile(String file, String str) {
         try {
-            FileUtils.writeStringToFile(new File(file),str);
+            FileUtils.writeStringToFile(new File(file), str);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String reverseString(String str){
-        return new StringBuilder(str).reverse().toString();
+    public String reverseString(String str) {
+        if (str == null){
+            str="";
+        }
+            return new StringBuilder(str).reverse().toString();
     }
 
-    public String readFile(String path, Charset encoding) throws IOException{
+    public String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
     }
