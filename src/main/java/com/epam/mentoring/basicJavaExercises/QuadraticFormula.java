5d2762f6ed82273c74d9acc71ed4cb6436c530b9
Adrian Balazs <adrian1468@gmail.com>
2018-06-17 17:24:33 +0200

    Created QuadraticFormula class

--- src/main/java/com/epam/mentoring/basicJavaExercises/QuadraticFormula.java
+++ src/main/java/com/epam/mentoring/basicJavaExercises/QuadraticFormula.java
@@ -1,4 +1,13 @@
 package com.epam.mentoring.basicJavaExercises;
 
 public class QuadraticFormula {
+
+    //Write a method that takes a, b, c integers as input and calculates the result of the ax2+bx+c=0 equation!
+
+    public double calcQuadraticFormula(int a, int b, int c) {
+        int dividend = b * b - 4 * a * c;
+        int divider = 2 * a;
+        return (-b + Math.sqrt(dividend) / divider);
+    }
+
 }
